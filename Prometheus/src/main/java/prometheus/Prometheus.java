9053e012e7eae983b23ffd6b4ca19a6e291d2b09
Vital Golub <vital.golub@uoit.net>
2019-03-26 13:39:45 -0400

    Gradle server error fix

--- Prometheus/src/main/java/prometheus/Prometheus.java
+++ Prometheus/src/main/java/prometheus/Prometheus.java
@@ -3,6 +3,7 @@ package prometheus;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.net.ServerSocket;
 import java.net.Socket;
 
 import javafx.application.Application;
@@ -10,6 +11,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import prometheus.constants.GlobalConstants;
 import prometheus.scenes.Sandbox;
+import prometheus.utils.FileUtils;
 
 public class Prometheus extends Application {
 	
@@ -17,9 +19,13 @@ public class Prometheus extends Application {
 	public static DataOutputStream toServer = null;
 	public static DataInputStream fromServer = null;
 	
+	static DataInputStream inputFromClient = null;
+	static DataOutputStream outputToClient = null;
+	
     @Override
     public void start(Stage primaryStage) {
     	try {
+    		runServer();
 			socket = new Socket("localhost", 2500); // set Socket 
 			fromServer = new DataInputStream(socket.getInputStream()); // set input stream
 			toServer = new DataOutputStream(socket.getOutputStream()); // set output stream
@@ -39,6 +45,33 @@ public class Prometheus extends Application {
 
     public static void main(String[] args) {
         launch(args);
+    }
+    
+    public static void runServer() {
+    	new Thread(() ->{
+    		System.out.println("Server started");
+    		try {
+    			ServerSocket serverSocket = new ServerSocket(2500);
+    			Socket socket = serverSocket.accept();
+    			
+    			inputFromClient = new DataInputStream(socket.getInputStream());
+    			outputToClient = new DataOutputStream(socket.getOutputStream());
+
+    			while (true) {
+    				if(inputFromClient != null && outputToClient != null) {
+    					String name = inputFromClient.readUTF();
+    					int kills = inputFromClient.readInt();
+    					int deaths = inputFromClient.readInt();
+    					
+    					System.out.println("Username: " + name + " Kills: " + kills + " Deaths: " + deaths);
+    					
+    					FileUtils.setPlayerStats(name, kills, deaths);
+    				}
+    			}
+    		} catch (IOException ex) {
+    			ex.printStackTrace();
+    		}
+    	}).start();
     }
 
 }
